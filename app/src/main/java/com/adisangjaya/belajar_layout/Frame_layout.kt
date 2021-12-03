8208135227346296425a9b3e3e71a44e6c156efb
AdiSangjaya <adi.sangjaya@gmail.com>
2021-12-03 23:07:45 +0700

    first commit

---
+++ app/src/main/java/com/adisangjaya/belajar_layout/Frame_layout.kt
@@ -0,0 +1,11 @@
+package com.adisangjaya.belajar_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Frame_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_layout)
+    }
+}
---
+++ app/src/main/java/com/adisangjaya/belajar_layout/Relatif_layout.kt
@@ -0,0 +1,11 @@
+package com.adisangjaya.belajar_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Relatif_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relatif_layout)
+    }
+}
